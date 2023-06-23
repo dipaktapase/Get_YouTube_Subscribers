e85b81d762989dc56fd3823e309b905043c6474b
dipaktapase <dipaktapase1512@gmail.com>
2023-06-23 16:23:08 +0530

    updated APIs and index.html files

--- src/app.js
+++ src/app.js
@@ -1,28 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const Subscribers = require("./models/subscribers");
 
-const express = require('express');
-const app = express()
+// Display homepage to the client
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
 
+// Fetching all the subscribers from the database
+app.get("/subscribers", async (req, res) => {
+  let subscribers = await Subscribers.find();
+  res.json(subscribers);
+});
 
-// Your code goes here
+// Fetching subscriber's name and the channel they subscribed
+app.get("/subscribers/names", async (req, res) => {
+  let subscribers = await Subscribers.find().select({
+    name: 1,
+    subscribedChannel: 1,
+    _id: 0,
+  });
+  res.json(subscribers);
+});
 
+// Fetching subscriber by their id from the database
+app.get("/subscribers/:_id", async (req, res) => {
+  let subscriberId = req.params._id;
+  await Subscribers.findById(subscriberId)
+    .then((subsId) => {
+      res.status(200).json(subsId);
+    })
+    .catch((err) => {
+      res.status(400).send({ message: err.message });
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Invalid page handling
+app.use((req, res) => {
+  res.status(404).send({ message: "Page Not Found" });
+});
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = require('./app.js')
-const mongoose = require('mongoose')
-const port = 3000
+const express = require("express");
+const app = require("./app.js");
+const mongoose = require("mongoose");
+const port = 3000;
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-db.once('open', () => console.log('connected to database'))
+// Connect to DATABASE to mongodb server
+const DATABASE_URL = "mongodb+srv://user12:almabetter123@cluster0.zxmw5rn.mongodb.net/subscribers";
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("connected to database"));
 
-// Start Server
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+// Start Server on port 3000
+app.listen(port, () => console.log(`App listening on port ${port}!`));
